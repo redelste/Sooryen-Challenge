9c77bc4edc757a49b34025d1cb7a1d102a6f37e9
redelste <redelste@stevens.sedu>
2019-04-05 18:49:43 -0400

    added scheduling to code. Runs at 3am every day

--- cragslistScraper.py
+++ cragslistScraper.py
@@ -5,22 +5,36 @@ import requests as rq
 import csv 
 
 
-base_url = "https://newyork.craigslist.org/search/sss?postal=07030"
+import schedule
+import time
 
-r = rq.get(base_url)
-soup = bsoup(r.content,'html5lib') # gets the HTML content of each page
-totalCount = int(soup.findAll('span', attrs={'class':'totalcount'})[0].string)
 
-results = []
-for num in range(0, totalCount, 120):
+def scrape():
+    '''Scrapes the craigslist web page at the given url.'''
+    base_url = "https://newyork.craigslist.org/search/sss?postal=07030"
+
     r = rq.get(base_url)
     soup = bsoup(r.content,'html5lib') # gets the HTML content of each page
-    cur_url = base_url + "&s=" + str(num)
-    resultInfo = soup.findAll('p',attrs={'class':'result-info'})
-    results.append(list(map(lambda x: (x.select('.result-title')[0].string, x.select('.result-price')[0].string), resultInfo)))
+    totalCount = int(soup.findAll('span', attrs={'class':'totalcount'})[0].string)
 
-flatten = lambda l: [item for sublist in l for item in sublist]
+    results = []
+    for num in range(0, totalCount, 120):
+        r = rq.get(base_url)
+        soup = bsoup(r.content,'html5lib') # gets the HTML content of each page
+        cur_url = base_url + "&s=" + str(num)
+        resultInfo = soup.findAll('p',attrs={'class':'result-info'})
+        results.append(list(map(lambda x: (x.select('.result-title')[0].string, x.select('.result-price')[0].string), resultInfo)))
 
-flat = flatten(results)
-for x in range(len(flat)):
-    print(x, flat[x])
+    flatten = lambda l: [item for sublist in l for item in sublist]
+
+    flat = flatten(results)
+    for x in range(len(flat)):
+        print(x, flat[x])
+
+
+''' Schedules the code to run at 3AM every day. '''
+schedule.every().day.at("03:00").do(scrape)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
